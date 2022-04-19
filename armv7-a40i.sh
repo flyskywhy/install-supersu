7674667dc22de9da6d5098c68a8365ce5dd8155e
Li Zheng <flyskywhy@gmail.com>
2022-04-19 16:48:20 +0800

    armv7-a40i-revert.sh: fix run error in `git bash` on Windows

--- armv7-a40i.sh
+++ armv7-a40i.sh
@@ -13,25 +13,25 @@ fi
 adb remount
 
 adb shell "mv /system/xbin/su /system/xbin/su_original"
-adb push armv7/su /system/xbin/su
+adb push armv7/su system/xbin/su
 # adb shell "chmod 4750 /system/xbin/su"
 adb shell "chmod 755 /system/xbin/su"
 adb shell "chown root:shell /system/xbin/su"
 adb shell "chcon u:object_r:system_file:s0 /system/xbin/su"
 
-adb push armv7/su /system/xbin/daemonsu
+adb push armv7/su system/xbin/daemonsu
 adb shell "chmod 755 /system/xbin/daemonsu"
 adb shell "chcon u:object_r:system_file:s0 /system/xbin/daemonsu"
 
 adb shell "mv /system/bin/app_process32 /system/bin/app_process32_original"
 adb shell "rm -rf /system/bin/app_process32"
 adb shell "ln -s /system/xbin/daemonsu /system/bin/app_process32"
-# adb shell chown root:root /system/bin/app_process32
+# adb shell "chown root:root /system/bin/app_process32"
 
-adb shell "/system/xbin/su --install"
+adb shell "system/xbin/su --install"
 
 adb install common/Superuser.apk
 
 # Seems need below and click on Android screen to run `SuperSU` APP
 # just after run this `.sh` to complete the root.
-adb shell "/system/xbin/su --daemon"
+adb shell "system/xbin/su --daemon"
